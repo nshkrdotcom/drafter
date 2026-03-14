4589ca602d53e917ef2e737befbe7cab439d209c
Jarius Jenkins <jarius.jenkins@gmail.com>
2026-03-14 15:23:21 +0900

    version bump

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Drafter.MixProject do
 
     [
       app: :drafter,
-      version: "0.1.5",
+      version: "0.1.6",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
