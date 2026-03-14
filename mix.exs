8230b4bfe5df4c93865150c71651cc18e0270ed9
Jarius Jenkins <jarius.jenkins@gmail.com>
2026-03-14 17:18:50 +0900

    version bump for number forma tfix.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Drafter.MixProject do
 
     [
       app: :drafter,
-      version: "0.1.9",
+      version: "0.1.10",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
